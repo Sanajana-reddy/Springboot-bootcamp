f4a9c64a457f4003889cdbed2b4b725f39dfbc25
Sanajana-reddy <reddysanjana385@gmail.com>
2026-06-15 23:03:19 +0530

    integrated HTML and CSS along with springboot project setup.

---
+++ src/main/java/com/sanjana/Springboot_bootcamp/SpringbootBootcampApplication.java
@@ -0,0 +1,13 @@
+package com.sanjana.Springboot_bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanjana/Springboot_bootcamp/SpringbootBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjana.Springboot_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
